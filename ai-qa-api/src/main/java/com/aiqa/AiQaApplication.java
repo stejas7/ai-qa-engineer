dfe255d8051906d6ba7422bdf969d0e6eb95fdfb
stejas7 <stejas7@gmail.com>
2026-08-17 11:18:31 +0530

    Add Spring Boot application

---
+++ ai-qa-api/src/main/java/com/aiqa/AiQaApplication.java
@@ -0,0 +1,11 @@
+package com.aiqa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiQaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AiQaApplication.class, args);
+    }
+}
