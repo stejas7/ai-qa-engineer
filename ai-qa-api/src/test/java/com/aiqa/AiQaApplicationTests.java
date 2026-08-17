938749e06320ead4c42b2ec36ee3e61c85e2ba33
stejas7 <stejas7@gmail.com>
2026-08-17 11:19:21 +0530

    Add starter test

---
+++ ai-qa-api/src/test/java/com/aiqa/AiQaApplicationTests.java
@@ -0,0 +1,6 @@
+package com.aiqa;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+class AiQaApplicationTests {
+ @Test void applicationClassExists(){assertNotNull(AiQaApplication.class);}
+}
