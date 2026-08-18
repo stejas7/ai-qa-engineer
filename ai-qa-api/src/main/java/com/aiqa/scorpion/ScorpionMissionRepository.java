e6157b2eec5c54a931ab3d9bc982fc5a71a17ce9
stejas7 <stejas7@gmail.com>
2026-08-18 11:28:20 +0530

    cleanup: remove Scorpion mission repository after Auravis rename

--- ai-qa-api/src/main/java/com/aiqa/scorpion/ScorpionMissionRepository.java
+++
@@ -1,6 +0,0 @@
-package com.aiqa.scorpion;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
-
-public interface ScorpionMissionRepository extends JpaRepository<ScorpionMission, UUID> {}
